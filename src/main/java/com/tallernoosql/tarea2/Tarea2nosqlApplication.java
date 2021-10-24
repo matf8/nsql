d1238b8cb32f3b62ec0a3b221dea4c83689892da
matf8 <mathias.fernandez.bo@fing.edu.uy>
2021-10-24 16:51:37 -0300

    update

---
+++ src/main/java/com/tallernoosql/tarea2/Tarea2nosqlApplication.java
@@ -0,0 +1,13 @@
+package com.tallernoosql.tarea2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2nosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2nosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tallernoosql/tarea2/Tarea2nosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallernoosql.tarea2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2nosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
